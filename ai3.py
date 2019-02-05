b027fb3da1cdd0b599354622c45aba0b1a1e5cee
Ubuntu <ubuntu@ip-172-26-5-227.us-east-2.compute.internal>
2019-02-05 05:08:58 +0000

    pull it now

--- ai3.py
+++ ai3.py
@@ -1,5 +1,6 @@
 from numpy import exp, array, random, dot
-from  matplotlib import pyplot as plt
+
+
 class NeuralNetwork():
     def __init__(self):
         # Seed the random number generator, so it generates the same numbers
@@ -10,6 +11,7 @@ class NeuralNetwork():
         # We assign random weights to a 3 x 1 matrix, with values in the range -1 to 1
         # and mean 0.
         self.synaptic_weights = 2 * random.random((2, 1)) - 1
+        print self.synaptic_weights
 
     # The Sigmoid function, which describes an S shaped curve.
     # We pass the weighted sum of the inputs through this function to
@@ -53,28 +55,25 @@ if __name__ == "__main__":
     #Intialise a single neuron neural network.
     neural_network = NeuralNetwork()
 
-    print ("Random starting synaptic weights: ")
-    print (neural_network.synaptic_weights)
+    print "Random starting synaptic weights: "
+    print neural_network.synaptic_weights
 
     # The training set. We have 4 examples, each consisting of 3 input values
     # and 1 output value.
-    training_set_inputs = array([[-1, -1], [-1, 1], [1, -1], [1, 11]])
+    training_set_inputs = array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
     training_set_outputs = array([[-1, 1, 1, 1]]).T
-    plt.plot(training_set_inputs, 'ro')
-    plt.axis('equal')
-    plt.show()
+
     # Train the neural network using a training set.
     # Do it 10,000 times and make small adjustments each time.
-    neural_network.train(training_set_inputs, training_set_outputs, 100000)
+    neural_network.train(training_set_inputs, training_set_outputs, 10000)
 
-    print ("New synaptic weights after training: ")
-    print (neural_network.synaptic_weights)
+    print "New synaptic weights after training: "
+    print neural_network.synaptic_weights
 
     # Test the neural network with a new situation.
-    print ("Considering new situation [1, 1] -> ?: ")
-    x = neural_network.think(array([-0.5, -0.5]))
-    print (x)
-    if x >= 0.5:
-        print (1)
-    else:
-        print (-1)
+    print "Considering new situation [1, 1] -> ?: "
+    print(neural_network.think(array([1, 1])
+    #if x > 1 : 
+    #  print -1
+    #else:
+    #  print 1	
