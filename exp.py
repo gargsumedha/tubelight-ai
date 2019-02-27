5189a1e4db374e4ace90244a4ec3eae54dfd7d7c
sumedhatubelight <gargs1@udayton.edu>
2019-02-26 22:40:55 -0500

    updated assignment 2

--- exp.py
+++ exp.py
@@ -1,106 +1,155 @@
-from numpy import exp, array, random, dot
-import matplotlib.pyplot as plt
+from numpy import exp, array, random, dot, all
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
-class NeuralNetwork():
+import sys
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib import cm
+import numpy
+from numpy.random import randn
+from scipy import array, newaxis
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+lr=0.001
+class CreateNet():
 	def __init__(self):
 		random.seed(1)
 
-		# setting the number of nodes in layer 2 and layer 3
-		# more nodes --> more confidence in predictions (?)
-		l2 = 3
-		l3 = 2
-
-		# assign random weights to matrices in network
-		# format is (no. of nodes in previous layer) x (no. of nodes in following layer)
-		self.synaptic_weights1 = 2 * random.random((3, l2)) -1
-		self.synaptic_weights2 = 2 * random.random((l2, l3)) -1
-		self.synaptic_weights3 = 2 * random.random((l3, 1)) -1
+		# The number of nodes in hidden layer 2 and hidden layer 3 are defined below
+		hidden_layer2 = 3
+		hidden_layer3 = 2
+		
+		# assigning random weights to 2D array in the network
+		self.weights1 = 2 * random.random((3, hidden_layer2)) -1
+		self.weights2 = 2 * random.random((hidden_layer2, hidden_layer3)) -1
+		self.weights3 = 2 * random.random((hidden_layer3, 1)) -1
 		
 	def __sigmoid(self, x):
 		return 1/(1+exp(-x))
 
-	# derivative of sigmoid function, indicates confidence about existing weight
+	
 	def __sigmoid_derivative(self, x):
 		return x*(1-x)
 
-	# train neural network, adusting synaptic weights each time
-	def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-		for iteration in range(number_of_training_iterations):
+	# training the network and adusting weights each time
+	def training(self, inputs, outputs, iterations):
+		print("Hello")
+		for i in range(iterations):
 
-			# pass training set through our neural network
-			# a2 means the activations fed to second layer
-			a2 = self.__sigmoid(dot(training_set_inputs, self.synaptic_weights1))
-			a3 = self.__sigmoid(dot(a2, self.synaptic_weights2))
-			output = self.__sigmoid(dot(a3, self.synaptic_weights3))
+			# passing the randomly generated training set through our neural network
+			# activation2 is the activations fed to second layer
+			activation2 = self.__sigmoid(dot(inputs, self.weights1))
+			activation3 = self.__sigmoid(dot(activation2, self.weights2))
+			output = self.__sigmoid(dot(activation3, self.weights3))
+			#print(output)
+			# error calculation
+			del4 = (outputs - output)*self.__sigmoid_derivative(output)
 
-			# calculate 'error'
-			del4 = (training_set_outputs - output)*self.__sigmoid_derivative(output)
+			# errors in each layer
+			del3 = dot(self.weights3, del4.T)*(self.__sigmoid_derivative(activation3).T)
+			del2 = dot(self.weights2, del3)*(self.__sigmoid_derivative(activation2).T)
 
-			# find 'errors' in each layer
-			del3 = dot(self.synaptic_weights3, del4.T)*(self.__sigmoid_derivative(a3).T)
-			del2 = dot(self.synaptic_weights2, del3)*(self.__sigmoid_derivative(a2).T)
+			# if we reach the required solution no need to feed forword
+			
+			# getting gradients for each layer
+			adj3 = dot(activation3.T, del4)
+			adj2 = dot(activation2.T, del3.T)
+			adj1 = dot(inputs.T, del2.T)
 
-			# get adjustments (gradients) for each layer
-			adjustment3 = dot(a3.T, del4)
-			adjustment2 = dot(a2.T, del3.T)
-			adjustment1 = dot(training_set_inputs.T, del2.T)
+			# adjusting weights accordingly
+			self.weights1 += adj1
+			self.weights2 += adj2
+			self.weights3 += adj3
 
-			# adjust weights accordingly
-			self.synaptic_weights1 += adjustment1
-			self.synaptic_weights2 += adjustment2
-			self.synaptic_weights3 += adjustment3
-
-	def forward_pass(self, inputs):
-		# pass our inputs through our neural network
-		a2 = self.__sigmoid(dot(inputs, self.synaptic_weights1))
-		a3 = self.__sigmoid(dot(a2, self.synaptic_weights2))
-		output = self.__sigmoid(dot(a3, self.synaptic_weights3)) 
+			
+	def forward(self, inputs):
+		# passing the inputs through the neural network
+		a2 = self.__sigmoid(dot(inputs, self.weights1))
+		a3 = self.__sigmoid(dot(a2, self.weights2))
+		output = self.__sigmoid(dot(a3, self.weights3)) 
 		return output
 
 if __name__ == "__main__":
-    # initialise single neuron neural network
-    neural_network = NeuralNetwork()
-    print("Random starting synaptic weights (layer 1): ")
-    print(neural_network.synaptic_weights1)
-    print("\nRandom starting synaptic weights (layer 2): ")
-    print(neural_network.synaptic_weights2)
-    print("\nRandom starting synaptic weights (layer 3): ")
-    print(neural_network.synaptic_weights3)
-    DataSet1 = [[0] * 3 for _ in range(100)]
-    DataSet2 = [[0] * 3 for _ in range(100)]
-    for i in range(100): 
-        DataSet1[i][0] = round(random.uniform(2,3),2)
-        DataSet1[i][1] = round(random.uniform(2,3),2)
-        DataSet1[i][2] = round(random.random(),3)
-    for i in range(100):
-        DataSet2[i][0] = round(random.uniform(0.5,1.5),2)
-        DataSet2[i][1] = round(random.uniform(0.5,1.5),2)
-        DataSet2[i][2] = round(random.random(),2)
+	output_s = [1]
+	DataSets = [[round(random.uniform(2,3),2),round(random.uniform(2,3),2),round(random.random(),3)]]
+	for i in range(1,100):
+		output_s.extend([1]) 
+		DataSets.insert(i,[round(random.uniform(2,3),2),round(random.uniform(2,3),2),round(random.random(),3)])
+	output_s.extend([0])
+	DataSets.insert(101,[round(random.uniform(0.5,1.5),2),round(random.uniform(0.5,1.5),2),round(random.random(),3)])
+	for i in range(102,200):
+		output_s.extend([0])
+		DataSets.insert(i,[round(random.uniform(0.5,1.5),2),round(random.uniform(0.5,1.5),2),round(random.random(),3)])
 
-  
-    # the training set.
+	# initialise single neuron neural network
+	for i in range(100):
+	    xs, ys, zs = DataSets[i]
+	    ax.scatter(xs, ys, zs, c="red", marker="^")
+	for i in range(101,199):
+	    xs, ys, zs = DataSets[i]
+	    ax.scatter(xs, ys, zs, c="green", marker="^")
 
-    for l in range(100):
-        training_set_inputs = array([ [ DataSet1[l][0],DataSet1[l][1],DataSet1[l][2] ], [ DataSet2[l][0],DataSet2[l][1],DataSet2[l][2] ] ])
-        training_set_outputs = array([[1,0]]).T
-        neural_network.train(training_set_inputs, training_set_outputs, 10000)
+	
+	neural_net = CreateNet()
+	print("Starting weights for layer 1, layer 2 and layer 3: ")
+	print(neural_net.weights1)
+	print("\n")
+	print(neural_net.weights2)
+	print("\n")
+	print(neural_net.weights3)
+	
+	inputs = array(DataSets)
+	outputs = array([output_s]).T
+	#print(inputs)
+	#print(outputs)
+	neural_net.training(inputs, outputs, 50000)
+	print ("\nNew weights for layer 1, layer 2 and layer 3 after training: ")
+	print (neural_net.weights1)
+	print ("\n")
+	print (neural_net.weights2)
+	print ("\n")
+	print (neural_net.weights3)
+    # testing with new input
 
 
-    print ("\nNew synaptic weights (layer 1) after training: ")
-    print (neural_network.synaptic_weights1)
-    print ("\nNew synaptic weights (layer 2) after training: ")
-    print (neural_network.synaptic_weights2)
-    print ("\nNew synaptic weights (layer 3) after training: ")
-    print (neural_network.synaptic_weights3)
-    # test with new input
-    ch = 'y'
-    while ch == 'y':
-        print ("\nConsidering new situation -> ?")
-        x1 =float( input("enter x coordinate"))
-        x2 =float( input("enter y coordinate"))
-        x3 =float(input("enter z coordinate"))
-        op= float(neural_network.forward_pass(array([x1,x2,x3])))
-        off = round(op,1)
-        print(off)
-        ch = input("press y to continue")
+	ch = 'y'
+	
+	while ch == 'y':
+		print ("\nCoordinates for new situation:")
+		x1 =float( input("enter x coordinate"))
+		x2 =float( input("enter y coordinate"))
+		x3 =float(input("enter z coordinate"))
+		ax.scatter(x1, x2, x3, c="blue", marker="^")
+		op= float(neural_net.forward(array([x1,x2,x3])))
+		print(op)
+		#off = round(op,1)
+		#if off <= 0.5:
+		#	print(0)
+		#else:
+		#	print (1)
+		#print(off)
+		ch = raw_input("press y to continue")
+	#DATA = array([ [0.08216108,  1.67946197, -4.02173908],[-1.82328547, -3.83638578,  0.7452409 ],[-2.7231652,  -0.56974737,  4.21862478]])
+	DATA = array([[neural_net.weights1[0][0],neural_net.weights1[0][1],neural_net.weights1[0][2]],
+	[neural_net.weights1[1][0],neural_net.weights1[1][1],neural_net.weights1[1][2]],
+	[neural_net.weights1[2][0],neural_net.weights1[2][1],neural_net.weights1[2][2]]
+	])
+	Xs = DATA[:,0]
+	Ys = DATA[:,1]
+	Zs = DATA[:,2]
+	surf = ax.plot_trisurf(Xs, Ys, Zs, cmap=cm.jet, linewidth=0)
+	fig.colorbar(surf)
+
+	ax.xaxis.set_major_locator(MaxNLocator(5))
+	ax.yaxis.set_major_locator(MaxNLocator(6))
+	ax.zaxis.set_major_locator(MaxNLocator(5))
+
+	fig.tight_layout()	
+	ax.set_xlabel('X Label')
+	ax.set_ylabel('Y Label')
+	ax.set_zlabel('Z Label')
+
+	plt.show()
